a39603499d89852a4202596c3ff0810d4d0ff0c7
Sagnik Mukherjee <samu825c@tu-dresden.de>
2022-10-04 14:01:11 +0200

    Sagnik's double comment commit

--- app/src/main/java/com/example/gitapp/MainActivity.java
+++ app/src/main/java/com/example/gitapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//Sagnik's comment on line 7
+//Sagnik's comment on line 8
+
 public class MainActivity extends AppCompatActivity {
 
  //this is our important change
